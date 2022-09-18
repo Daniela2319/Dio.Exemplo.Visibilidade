b37edc1ac77b22d57b35fe0c99d090f61a277f89
Daniela2319 <frankvt04@gmail.com>
2022-09-18 20:46:53 -0300

    Exemplos visibilidade

---
+++ ExemploPacote/src/Lanchonete/Atendimento/Cozinha/Almoxerife.java
@@ -0,0 +1,19 @@
+package Lanchonete.Atendimento.Cozinha;
+
+public class Almoxerife {
+	public void controlarEntrada() {
+		System.out.println("Controlando a entrada");
+		
+	}
+	public void controlarSaida() {
+		System.out.println("Controlando a saida");
+		
+	}
+	public void entregarIngrediente() {
+		System.out.println("Entregando ingrediente");
+	}
+	private void trocarGas() {
+		System.out.println("Almoxerife trocar o gás");
+	}
+
+}
